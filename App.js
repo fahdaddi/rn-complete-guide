f962d1fdbf55d2d88f087061191ec92e8be63bf2
Fahd <fahd.addi@gmail.com>
2020-05-10 21:15:34 +0000

    24. Inline Styles & StyleSheet Objects

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ paddingTop: 50, paddingHorizontal: 10 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'baseline' }}>
-        <TextInput placeholder="Course Goal" style={{ flex: 1, borderBottomColor: 'black', borderBottomWidth: 2, marginRight: 10}}/>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.inputText} />
         <Button title="Add" />
       </View>
-      <View>
-
-      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "baseline",
+  },
+  inputText: {
+    flex: 1,
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    marginRight: 10,
+  },
 });
