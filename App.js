b70deb5ec7353f96f442441c8ac3913f1cc806ec
Fahd <fahd.addi@gmail.com>
2020-05-10 23:09:38 +0000

    adding items and outputing the list items logic

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const handleInput = (text) => {
+    setEnteredGoal(text);
+  };
+
+  const addGoal = () => {
+    if (!!enteredGoal.length) {
+      setGoals((currentGoals) => [...currentGoals, enteredGoal]);
+      setEnteredGoal("");
+    }
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.inputText} />
-        <Button title="Add" />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.inputText}
+          onChangeText={handleInput}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoal} />
       </View>
-      <View></View>
+      <View>
+        {goals.map((goal) => (
+          <Text key={goal} >{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
